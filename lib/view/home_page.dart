cac5d89709f2e9d3f196940b5d8aca00337109d2
Umut <92772541+OmidHaqi@users.noreply.github.com>
2024-01-28 07:06:14 +0330

    Adds imports and implements bloc pattern for handling article loading states, including displaying a loading indicator, a list of articles, and an error message if necessary in the home page.

---
+++ lib/view/home_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../bloc/app_event.dart';
+import '../bloc/app_state.dart';
+import '../bloc/blocs.dart';
+import '../repo/repo.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (BuildContext context) => ArticleBLoc(
+            RepositoryProvider.of<Repository>(context)
+        )..add(LoadArticleEvent()),
+        child: Scaffold(
+
+            body:  BlocBuilder<ArticleBLoc,ArticleState>(
+              builder: (context, state) {
+
+                if (state is ArticleLoadingState) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                //- اگر وضعیت `ArticleLoadingState` باشد، یعنی بلوک در حال بارگذاری مقالات است، یک نمایشگر `CircularProgressIndicator` که یک نشانگر پیشرفت است نمایش داده می‌شود. این نشانگر معمولاً برای نشان دادن اینکه عملیات بارگذاری در حال انجام است استفاده می‌شود.
+                if (state is ArticleLoadedState) {
+                  List<Article> articlesList= state.articles;
+
+                  return ListView.builder(
+                    itemCount: articlesList.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(24),
+                        child: Card(child: Text(articlesList[index].title),),
+                      );
+                    },
+                  );
+
+                }
+
+                //- اگر وضعیت `ArticleLoadedState` باشد، یعنی مقالات با موفقیت بارگذاری شده‌اند، لیستی از مقالات در یک `ListView` نمایش داده می‌شود. از `ListView.builder` استفاده می‌شود تا بتوان به طور پویا و به اندازه‌ی لازم عناصر را در لیست نمایش داد.
+                if (state is ArticleErrorState) {
+                  String  error= state.error;
+
+                  return Center(
+                    child: Text(error),
+                  );
+
+                }
+
+                //- در غیر این صورت، یعنی وضعیت دیگری به جز `ArticleLoadingState` و `ArticleLoadedState` است، یک `Container` خالی نمایش داده می‌شود.
+                return Text("no data");
+              },)
+        ));
+  }
+}
