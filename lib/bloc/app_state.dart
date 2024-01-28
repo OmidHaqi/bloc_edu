d4422b6be75330cc789d9b7faa5963e12ca09b7b
Umut <92772541+OmidHaqi@users.noreply.github.com>
2024-01-28 06:58:33 +0330

    Add ArticleState, ArticleLoadingState, ArticleLoadedState, and ArticleErrorState classes to app_state.dart file, which extends Equatable class and have props implementation.

---
+++ lib/bloc/app_state.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+
+import '../model/article_model.dart';
+
+abstract class ArticleState extends Equatable {}
+
+//loading state
+
+class ArticleLoadingState extends ArticleState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+//data loaded state
+
+class ArticleLoadedState extends ArticleState {
+  ArticleLoadedState(this.articles);
+  final List<Article> articles;
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [articles];
+}
+
+//data error loading state
+
+class ArticleErrorState extends ArticleState {
+  ArticleErrorState(this.error);
+  final String error;
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [error];
+}
